cbc28c3a6cfc92ffd0bd572874a7bbea7c18e3a8
xxsgithub <wxxzhuang@gmail.com>
2016-09-08 03:20:26 +0800

    增加手势事件
    ...

--- js/mapp.js
+++ js/mapp.js
@@ -2,7 +2,12 @@
     var width,   //  视口宽度
         height,   //  视口高度
         timer,   //  nav-btn的计时器
-        id;      //  move的计时器
+        id,      //  move的计时器
+        position = [],  //  块的位置
+        scroll_pos = [], //  块移动目标位置
+        block,   //  块
+        index,  //  当前位置
+        hammertime;   //  手势
 
     //  改变视口大小时的调整
     function change_size() {
@@ -12,9 +17,35 @@
             .css("width", width);
         $("#background_shadow").css("height", height)
             .css("width", width);
+        calculate(6);
     }
 
-    //增加nav点击事件、返回顶部。优化部分效果
+//  计算各个块的位置
+    function calculate(num) {
+        var temph;  // 块的高度
+        var tempt;  // 块的offsetTop
+        for (var i = 0; i < num; i++) {
+            temph = (height - block[i].clientHeight) / 2;
+            tempt = block[i].offsetTop;
+            position[i] = tempt - 40;
+            if (temph < 0) {
+                scroll_pos[i] = tempt;
+            } else {
+                scroll_pos[i] = tempt - temph;
+            }
+        }
+    }
+
+    //  获取当前所处位置
+    function get_position(num) {
+        var current = $(document).scrollTop();
+        for (var i = 0; i < num; ++i) {
+            if (current < position[i]) {
+                return i;
+            }
+        }
+        return 6;
+    }
 
     //  移动滚动条
     function move(current, step) {
@@ -23,7 +54,7 @@
         id = setInterval(function () {
             window.scrollTo(0, current - step);
             ++t;
-            current -=  step;
+            current -= step;
             if (t >= 30) {
                 clearInterval(id);
             }
@@ -35,7 +66,7 @@
         $("#drop-nav").hide();
         var aim = $($(event.target).attr("href"))[0].offsetTop;
         var current = $(document).scrollTop();
-        move(current,(current - aim) / 30);
+        move(current, (current - aim) / 30);
         event.preventDefault();
     }
 
@@ -46,27 +77,27 @@
         var temp = parseInt(nav.css("height"));
         console.log(temp);
         if (temp > 0) {
-            timer=setInterval(function () {
-                if (temp<20){
-                    nav.css("height",0);
-                    nav.css("display","none");
+            timer = setInterval(function () {
+                if (temp < 20) {
+                    nav.css("height", 0);
+                    nav.css("display", "none");
                     clearInterval(timer);
                     return;
                 }
-                temp-=20;
-                nav.css("height",temp);
-            },15);
+                temp -= 20;
+                nav.css("height", temp);
+            }, 15);
         } else {
-            nav.css("display","block");
-            timer=setInterval(function () {
-                if (temp>150){
-                    nav.css("height",215);
+            nav.css("display", "block");
+            timer = setInterval(function () {
+                if (temp > 150) {
+                    nav.css("height", 215);
                     clearInterval(timer);
                     return;
                 }
-                temp+=20;
-                nav.css("height",temp);
-            },15);
+                temp += 20;
+                nav.css("height", temp);
+            }, 15);
         }
     }
 
@@ -82,9 +113,58 @@
         gt = null;
     }
 
+    //手势上滑事件
+    function swipeTop() {
+        console.log("swipeTop");
+        var endCurrent = $(document).scrollTop();  //  滑动停止时的位置
+        var temp, step;  //  temp为目标位置，step为每一次移动的距离
+
+        if (index == 5) {
+            move(endCurrent, (endCurrent - scroll_pos[0]) / 30);
+        } else {
+            temp = scroll_pos[index+1];
+            step = (endCurrent - temp) / 30;
+            move(endCurrent, step);
+        }
+    }
+
+    //  手势下滑事件
+    function swipeDown() {
+        console.log("swipeDown");
+        var endCurrent = $(document).scrollTop();  //  滑动停止时的位置
+        var temp, step;  //  temp为目标位置，step为每一次移动的距离
+
+        if (index == 0) {
+            temp = scroll_pos[5];
+            step = (endCurrent - temp) / 30;
+            move(endCurrent, step);
+        } else {
+            temp = scroll_pos[index - 1];
+            step = (endCurrent - temp) / 30;
+            move(endCurrent, step);
+        }
+    }
+
+
     $(document).ready(function () {
+        block = $(".container-fluid").children();
+        console.log("compare");
+        hammertime = new Hammer(document.getElementById("swipe"), {
+            recognizers: [
+                [Hammer.Swipe]
+            ]
+        });
+        console.log("init");
+        index = get_position(6);
+
         change_size();
         change_gt();
+
+        //  手势下滑事件
+        hammertime.on("swipedown",swipeDown);
+
+        //  手势上滑事件
+        hammertime.on("swipeup",swipeTop);
 
         //  改变窗口大小事件
         $(window).resize(change_size);
@@ -98,13 +178,14 @@
         //  go-tio点击事件
         $("#go_top").click(function () {
             var current = $(document).scrollTop();
-            move(current,current / 30);
+            move(current, current / 30);
             event.preventDefault();
         });
 
         //  当滚动停止时的操作
         $(window).bind('scrollstop', function () {
             change_gt();
+            index = get_position(6);
         });
 
     });
