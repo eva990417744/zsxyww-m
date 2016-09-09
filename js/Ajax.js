dfe9c1d14cca735b42ab36e8759cc1f36177b456
LunziQwQ <541643107@qq.com>
2016-09-09 19:33:35 +0800

    吃饭去了，回来再嗨，给html里面的form都加了name属性便于序列化

--- js/Ajax.js
+++ js/Ajax.js
@@ -7,29 +7,34 @@ ajax = new Ajax();
 ajax.getKey();
 
 function Ajax() {
+    var QuQ = this;
     var key;
     var loginkey;
     var freshMenList;
     this.getKey = function () {
         $.get("http://localhost:8888/api/", {}, function (data) {
-            ajax.key = data.key;
+            QuQ.key = data.key;
         });
     };
 
     this.submitInfo = function(){
-        $.post("http://localhost:8888/api/apply?k=" + ajax.key, studentInfo, function (data) {
+        var studentInfo = $("#baoming1 form").serialize();
+        $.post("http://localhost:8888/api/apply?k=" + QuQ.key, studentInfo, function (data) {
             if(data.success) {
                 //提交成功
+                console.log("success");
             }else {
                 //提交失败，显示错误信息
+                console.log(data.error);
             }
         }, "json");
+        return false;
     };
 
     this.login = function () {
         $.post("http://local:8888/api", account, function (data) {
             if(data.key) {
-                ajax.key = data.key;
+                QuQ.key = data.key;
             }else {
                 //验证失败，显示错误信息；
             }
@@ -38,9 +43,10 @@ function Ajax() {
 
     this.getInfoList = function () {
         $.get("http://local:8888/api/list?k=" + loginkey, {}, function (data) {
-            ajax.freshMenList = data;       //获取报名新生数据
+            QuQ.freshMenList = data;       //获取报名新生数据
         });
     };
 
-    $("#baoming1 > .button").addEventListener("click", ajax.submitInfo, false);
+    var sumbitForm = $("#baoming1");
+    sumbitForm.submit(QuQ.submitInfo);
 }
