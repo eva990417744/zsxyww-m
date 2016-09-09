99bd360d89f4ab652807e012db211d00d9c43f2f
eva990417744 <990417744@qq.com>
2016-09-09 20:14:21 +0900

    Revert "Merge branch 'master' of https://github.com/eva990417744/zsxyww-m"
    This reverts commit 4ed6c42252f1e1e0b144751a250409ff8a971855.

--- js/mapp.js
+++ js/mapp.js
@@ -1,38 +1,72 @@
 (function () {
-    var maxPage = 6, //  最大页数
+    var width,   //  视口宽度
+        height,   //  视口高度
         timer,   //  nav-btn的计时器
+        id,      //  move的计时器
+        position = [],  //  块的位置
+        scroll_pos = [], //  块移动目标位置
+        block,   //  块
         index,  //  当前位置
         hammertime;   //  手势
 
     //  改变视口大小时的调整
     function change_size() {
-        var width = $(window).width();
-        var height = $(window).height();
+        width = $(window).width();
+        height = $(window).height();
         $("#background_img").css("height", height)
             .css("width", width);
         $("#background_shadow").css("height", height)
             .css("width", width);
+        calculate(6);
     }
 
-    //  改变背景图片
-    function change_bgimg() {
-        $("#background_img").attr("src", "img/bg" + index + ".jpg")
+    //  计算各个块的位置
+    function calculate(num) {
+        var temph;  // 块的高度
+        var tempt;  // 块的offsetTop
+        for (var i = 0; i < num; i++) {
+            temph = (height - block[i].clientHeight) / 2;
+            tempt = block[i].offsetTop;
+            position[i] = tempt;
+            if (temph < 0) {
+                scroll_pos[i] = tempt;
+            } else {
+                scroll_pos[i] = tempt - temph;
+            }
+        }
     }
 
-    //  获得当前位置
+    //  获取当前所处位置
     function get_position(num) {
-        for (var i = 1; i <= num; ++i) {
-            if ($("#page" + i).css("display") != 'none') {
+        var current = $(document).scrollTop();
+        for (var i = 0; i < num; ++i) {
+            if (current < position[i]) {
                 return i;
             }
         }
+        return 6;
+    }
+
+    //  移动滚动条
+    function move(current, step) {
+        var t = 0;
+        clearInterval(id);
+        id = setInterval(function () {
+            window.scrollTo(0, current - step);
+            ++t;
+            current -= step;
+            if (t >= 30) {
+                clearInterval(id);
+            }
+        }, 12.5);
     }
 
     // nav点击事件
     function click_nav(event) {
         $("#drop-nav").hide();
-        var aim = $(event.target).attr("href").slice(5);
-        change_page(aim, index);
+        var aim = $($(event.target).attr("href"))[0].offsetTop;
+        var current = $(document).scrollTop();
+        move(current, (current - aim) / 30);
         event.preventDefault();
     }
 
@@ -41,6 +75,7 @@
         clearInterval(timer);
         var nav = $("#drop-nav");
         var temp = parseInt(nav.css("height"));
+        console.log(temp);
         if (temp > 0) {
             timer = setInterval(function () {
                 if (temp < 20) {
@@ -68,54 +103,64 @@
 
     // 控制go-tio的显示与隐藏
     function change_gt() {
+        var pos = $(document).scrollTop();
         var gt = $("#go_top");
-        index == 1 ? gt.hide() : gt.show();
-    }
-
-    //  切换页面
-    function change_page(aim, current) {
-        $("#page" + current).hide();
-        $("#page" + aim).show();
-        index = aim;
-        change_gt();
-        change_bgimg();
-    }
-
-    //  手势左滑事件
-    function swipeLeft() {
-        if (index >= maxPage) {
-            return;
+        if (pos > height / 3) {
+            gt.fadeIn();
+        } else {
+            gt.fadeOut();
         }
-        change_page(index + 1, index);
+        gt = null;
     }
 
-    //  手势右滑事件
-    function swipeRight() {
-        if (index <= 1) {
-            return;
+    //手势上滑事件
+    function swipeTop() {
+        var endCurrent = $(document).scrollTop();  //  滑动停止时的位置
+        var temp, step;  //  temp为目标位置，step为每一次移动的距离
+
+        if (index >= 5) {
+            move(endCurrent, (endCurrent - scroll_pos[0]) / 30);
+        } else {
+            temp = scroll_pos[index + 1];
+            step = (endCurrent - temp) / 30;
+            move(endCurrent, step);
         }
-        change_page(index - 1, index);
+    }
+
+    //  手势下滑事件
+    function swipeDown() {
+        var endCurrent = $(document).scrollTop();  //  滑动停止时的位置
+        var temp, step;  //  temp为目标位置，step为每一次移动的距离
+
+        if (index == 0) {
+            temp = scroll_pos[5];
+            step = (endCurrent - temp) / 30;
+            move(endCurrent, step);
+        } else {
+            temp = scroll_pos[index - 1];
+            step = (endCurrent - temp) / 30;
+            move(endCurrent, step);
+        }
     }
 
 
     $(document).ready(function () {
+        block = $(".container-fluid").children();
         hammertime = new Hammer(document.getElementById("swipe"), {
             recognizers: [
                 [Hammer.Swipe]
             ]
         });
+        index = get_position(6);
 
-        //  初始化各个东西
-        index = get_position(maxPage);
         change_size();
         change_gt();
-        change_bgimg();
 
-        //  手势左滑事件
-        hammertime.on("swipeleft", swipeLeft);
+        //  手势下滑事件
+        hammertime.on("swipedown", swipeDown);
 
-        //  手势右滑事件
-        hammertime.on("swiperight", swipeRight);
+        //  手势上滑事件
+        hammertime.on("swipeup", swipeTop);
 
         //  改变窗口大小事件
         $(window).resize(change_size);
@@ -128,8 +173,15 @@
 
         //  go-tio点击事件
         $("#go_top").click(function () {
-            change_page(1, index);
+            var current = $(document).scrollTop();
+            move(current, current / 30);
             event.preventDefault();
+        });
+
+        //  当滚动停止时的操作
+        $(window).bind('scrollstop', function () {
+            change_gt();
+            index = get_position(6);
         });
 
     });
